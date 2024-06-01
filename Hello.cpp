539e94bed8ff250f1055dbaef9832fdc944f9fc1
Gokul7510 <gokulgopinath7510@gmail.com>
2024-06-01 18:26:39 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,6 +1,43 @@
-#include <iostream.h>
+#include <iostream>
+#include <string>
 
+int Multiply(int a, int b)
+{
+	return a * b;
+}
+int Add(int a, int b)
+{
+	return a + b;
+}
+int Divide(int a, int b)
+{
+	return a / b;
+}
+int Substract(int a, int b)
+{
+	return a - b;
+
+}
+void Getresult(int a, int b)
+{
+	int add_res = Add(a, b);
+	int sub_res = Substract(a, b);
+	int div_res = Divide(a, b);
+	int mult_res = Multiply(a, b);
+	std::cout << add_res << std::endl;
+	std::cout << sub_res << std::endl;
+	std::cout << div_res << std::endl;
+	std::cout << mult_res << std::endl;
+}
+void Getoptions()
+{
+	std::cout << "enter the input" << std::endl;
+
+	if(hoice==5)
+}
 int main()
 {
-    std::cout<<"Hello"<<std::endl;
+	Getresult(3, 4);
+
 }
+
